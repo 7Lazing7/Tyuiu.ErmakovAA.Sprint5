970574d1630d95b14d9e4598f7794481fd8740a4
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-12-12 01:55:53 +0500

    Task4

--- Tyuiu.ErmakovAA.Sprint5.Task4.V19.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint5.Task4.V19.Lib/DataService.cs
@@ -8,9 +8,9 @@ namespace Tyuiu.ErmakovAA.Sprint5.Task4.V19.Lib
         {
             string strX = File.ReadAllText(path);
             strX = strX.Replace('.', ',');
-            double ret = Math.Round (Math.Pow((Convert.ToDouble (strX) / Math.Cos(Convert.ToDouble(strX))), 2),3);
-           
-            return ret;
+            double res = Math.Pow((Convert.ToDouble (strX) / Math.Cos(Convert.ToDouble(strX))), 2);
+            res = Math.Round (res, 3);
+            return res;
         }
     }
 }
