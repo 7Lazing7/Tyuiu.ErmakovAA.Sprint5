ece68dd975eff785e5a90c5339f33c52f70b89f9
Alexsandr Ermokov <Tyuiu.ErmakovA.A@outlook.com>
2024-12-12 11:51:39 +0500

    Task 5

--- Tyuiu.ErmakovAA.Sprint5.Task5.V2.Lib/DataService.cs
+++ Tyuiu.ErmakovAA.Sprint5.Task5.V2.Lib/DataService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
+
 using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.ErmakovAA.Sprint5.Task5.V2.Lib
 {
     public class DataService : ISprint5Task5V2
@@ -7,48 +9,28 @@ namespace Tyuiu.ErmakovAA.Sprint5.Task5.V2.Lib
 
         public double LoadFromDataFile(string path)
         {
-            try
+            double res = 0;
+            double k = 0;
+            using (StreamReader sr = new StreamReader(path))
             {
-                // Чтение всех строк из файла
-                string[] lines = File.ReadAllLines(path);
-                double sum = 0;
-                int count = 0;
-
-                foreach (var line in lines)
+                string? line;
+                while ((line = sr.ReadLine()) != null)
                 {
-                    // Пробуем преобразовать строку в число
-                    if (double.TryParse(line.Trim(), NumberStyles.Float, CultureInfo.InvariantCulture, out double number))
+                    string[] numbers = line.Split(' ');
+                    foreach (string number in numbers)
                     {
-                        // Проверяем, является ли число положительным
-                        if (number > 0)
+                        double number_ = double.Parse(number, CultureInfo.InvariantCulture);
+                        if (number_ != Convert.ToInt32(number_))
                         {
-                            sum += number;
-                            count++;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Не удалось преобразовать строку: '{line.Trim()}'");
-                    }
-                }
+                            k += 1;
+                            res = res + Math.Round(number_, 3);
 
-                // Вычисление среднего значения
-                if (count > 0)
-                {
-                    double average = sum / count;
-                    return Math.Round(average, 3);
-                }
-                else
-                {
-                    Console.WriteLine("Нет положительных чисел для вычисления среднего.");
-                    return 0; // Если нет положительных чисел
+                        }
+
+                    }
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Ошибка при чтении файла: {e.Message}");
-                return 0; // Возвращаем 0 в случае ошибки
-            }
+            return Math.Round(res / k, 3);
         }
     }
 }
